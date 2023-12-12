1d05267a85ef176845bfdc7bc960e85d3effb86d
MiguelUCACUE <151780549+MiguelUCACUE@users.noreply.github.com>
2023-12-12 09:44:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+# Realice las siguientes operaciones:
+        # Operaciones Básicas y Tipos de Datos
+    #Suma
+a = 5 #Variable
+b = 3 #Variable
+print("Suma a y b es igual a", a+b) # V
+c = 93 #Variable
+d = 21 #Variable
+print("Suma c y d es igual a", c+d)
+print("------------------------------------------------") #Separacion de texto
+
+    #Resta
+e = 7.5 #Variable
+f = 2.5 #Variable
+print("Resta e y f es igual a", e-f)
+g = 8.4 #Variable
+h = 9.7 #Variable
+print("Resta g y h es igual a", g-h)
+print("------------------------------------------------") #Separacion de texto
+
+    #Multiplicación
+i = 5*6 #Variable
+print("Multiplicación i es igual a", i)
+j = 7*3 #Variable
+print("Multiplicación j es igual a", j)
+k = 96*365 #Variable
+print("Multiplicación k es igual a", k)
+l = 96*45 #Variable
+print("Multiplicación l es igual a", l)
+print("------------------------------------------------") #Separacion de texto
+
+    #División
+m = 5.3/7.8 #Variable
+print("Division m es igual a", i)
+n = 9.4/8.9 #Variable
+print("Division n es igual a", j)
+o= 8.7/5.7 #Variable
+print("Division o es igual a", o)
+p = 6.4/32 #Variable
+print("Division p es igual a", p)
+print("------------------------------------------------") #Separacion de texto
+
+    #Manipulación de Cadenas de Texto
+
+nombre = "Juan Miguel Cajamarca Avila" #Variable definida del nombre
+print("Mi nombre completo es", nombre) #Variable definida del nombre con print
+print("Los caracteres de mi nombre completo son", len(nombre)) #Variable definida del nombre, con funcion len, mostrando los caracteres totales
+print("------------------------------------------------") #Separacion de texto
+
+    #Conversión de Tipos de Datos
+# Conversión de entero a flotante y viceversa
+entero = 8 #Variable entera
+flotante = 5.4 #Variable flotante
+flotante_a_entero = float(entero) #Cambio de tipo
+entero_a_flotante = int(flotante) #Cambio de tipo
+print("La coversion original 1 es igual a", type(flotante_a_entero)) #Tipo de class
+print("La coversion original 2 es igual a", type(entero_a_flotante)) #Tipo de class
+print("------------------------------------------------")  #Separacion de texto
+
+# Impresión de tipos de datos
+print("El tipo de dato original de la variable entero es:", type(entero)) #Tipo de class
+print("El tipo de dato después de la conversión a flotante es:", type(flotante_a_entero)) #Tipo de class
+print("El tipo de dato original de la variable flotante es:", type(float)) #Tipo de class
+print("El tipo de dato después de la conversión a entero es:", type(entero_a_flotante))#Tipo de class
